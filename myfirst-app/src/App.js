3ac208ebb52109bb9f89e8e1fd2ede4a2a3d2a95
Sufiyan33 <basu700736@gmail.com>
2024-02-08 16:23:51 +0530

    video-10 Enabling dark mode in container '

--- myfirst-app/src/App.js
+++ myfirst-app/src/App.js
@@ -1,12 +1,14 @@
+import About from "./components/About";
 import Navbar from "./components/Navbar";
-import TextForms from "./components/TextForms";
+//import TextForms from "./components/TextForms";
 
 function App() {
   return (
     <>
     <Navbar tittle = "TextUtils3" textAbout = "About TextUtiles"/>
     <div className="container my-3">
-      <TextForms heading = "Enter the text here to analyze... "/>
+      {/*<TextForms heading = "Enter the text here to analyze... "/>*/}
+      <About/>
     </div>
   </>
   );
